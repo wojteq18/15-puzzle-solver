80c5fa0ac0fdca1288e215569b0422118dadb6ee
wojteq18 <279730@student.pwr.edu.pl>
2025-04-16 01:00:36 +0200

    swaps done

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,6 @@
 use crate::constants::{SIZE, PUZZLE_SIZE, MOVABLE_PIECE};
+//use rand::Rng;
+use rand::seq::SliceRandom; // Importujemy SliceRandom dla metody shuffle
 
 #[derive(Clone, Copy, Debug)]
 
@@ -11,10 +13,6 @@ impl Field {
     pub fn value(&self) -> usize {
         self.value
     }
-
-    pub fn index(&self) -> usize {
-        self.index
-    }
 }
 
 pub struct Board {
@@ -23,22 +21,27 @@ pub struct Board {
 }
 
 impl Board {
-    pub fn find_movable_piece(&self) -> Vec<Field> {
+
+    pub fn get_zero_element(&self) -> usize {
+        return self.zero_position;
+    }
+
+    pub fn find_movable_piece(&self) -> Vec<usize> {
         let mut movable_piece = Vec::new();
         let col = self.zero_position % SIZE;
         let row = self.zero_position / SIZE;
 
         if row > 0 {
-            movable_piece.push(self.fields[self.zero_position - SIZE]);
+            movable_piece.push(self.fields[self.zero_position - SIZE].index);
         }
         if row < SIZE - 1 {
-            movable_piece.push(self.fields[self.zero_position + SIZE]);
+            movable_piece.push(self.fields[self.zero_position + SIZE].index);
         }
         if col > 0 {
-            movable_piece.push(self.fields[self.zero_position - 1]);
+            movable_piece.push(self.fields[self.zero_position - 1].index);
         }
         if col < SIZE - 1 {
-            movable_piece.push(self.fields[self.zero_position + 1]);
+            movable_piece.push(self.fields[self.zero_position + 1].index);
         }
 
         return movable_piece
@@ -46,7 +49,7 @@ impl Board {
 
     pub fn new() -> Self {
         let mut fields = [Field { value: 0, index: 0 }; PUZZLE_SIZE];
-        let zero_position = 4; // The position of the empty space (0)
+        let zero_position = 8; // The position of the empty space (0)
 
         for i in 0..PUZZLE_SIZE {
             fields[i] = Field { value: (i + 1) % PUZZLE_SIZE, index: i };
@@ -63,5 +66,31 @@ impl Board {
             }
             println!();
         }
+    }
+
+    pub fn shuffle(&mut self) {
+        let mut numbers: Vec<usize> = (1..PUZZLE_SIZE).collect();
+        let mut rng = rand::rng();
+        numbers.shuffle(&mut rng);
+        
+        for i in 0..PUZZLE_SIZE - 1 {
+            self.fields[i].value = numbers[i];   
+        }
+    }
+
+    pub fn swap(&mut self, index1: usize, index2: usize) {
+        let value1 = self.fields[index1].value;
+        let value2 = self.fields[index2].value;
+    
+        self.fields.swap(index1, index2);
+    
+        if value1 == 0 {
+            self.zero_position = index2;
+        } else if value2 == 0 {
+            self.zero_position = index1;
+        }
+    
+        self.fields[index1].index = index1;
+        self.fields[index2].index = index2;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use board::*;
 
 fn main() { 
     let mut board = Board::new();
+    //board.print();
+    board.shuffle();
     board.print();
     let mut mov = board.find_movable_piece();
     println!("Movable pieces: {:?}", mov);
+    println!("{}", board.get_zero_element());
+    board.swap(board.get_zero_element(), mov[0]);
+    board.print();
+    let mov1 = board.find_movable_piece();
+    println!("Movable pieces: {:?}", mov1);
+    println!("{}", board.get_zero_element());
 }
